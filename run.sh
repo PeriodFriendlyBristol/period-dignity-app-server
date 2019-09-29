1800c79403e0fe430fec67adc1aa24c48bd513db
Adam Waterman <awaterman5895@gmail.com>
2019-09-29 12:36:07 +0100

    run script and first migrations

---
+++ run.sh
@@ -0,0 +1,5 @@
+
+python ./server/manage.py migrate
+
+python ./server/manage.py runserver 0.0.0.0:8000
+
--- venue/views.py
+++ venue/views.py
@@ -2,10 +2,13 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Venue
+
 
 # Create your views here.
 
 class VenueApi(APIView):
     def get(self, request):
+        queryset = Venue.get.all()
         data = {'location': 'BTP'}
         return Response(data)
