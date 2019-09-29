182c7f3453dfda5e24445abd212bd3892e17e121
Adam Waterman <awaterman5895@gmail.com>
2019-09-29 13:01:47 +0100

    fixed models names

--- contact/views.py
+++ contact/views.py
@@ -2,7 +2,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Venue
+from .models import Contact
 
 
 # Create your views here.
@@ -11,8 +11,8 @@ class ContactsApi(APIView):
     '''
     The Docs go here
     '''
+
     def get(self, request):
         queryset = Venue.get.all()
-        data = {'phone': 071234 }
+        data = {'phone': '07123456789'}
         return Response(data)
-
