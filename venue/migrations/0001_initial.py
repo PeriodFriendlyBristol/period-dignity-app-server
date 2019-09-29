2a6a24d1d49c0aef95ee0fe58e06f9dad0d74a95
Adam Waterman <awaterman5895@gmail.com>
2019-09-29 12:52:18 +0100

    example import data hook

--- venue/migrations/0001_initial.py
+++ venue/migrations/0001_initial.py
@@ -2,6 +2,12 @@
 
 from django.db import migrations, models
 
+def import_csv_data(apps, schema_editor):
+    # We can't import the Person model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+    venue = apps.get_model('venue', 'Venue')
+
+
 
 class Migration(migrations.Migration):
 
@@ -18,4 +24,5 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(default='Default', max_length=80)),
             ],
         ),
+        migrations.RunPython(import_csv_data),
     ]
