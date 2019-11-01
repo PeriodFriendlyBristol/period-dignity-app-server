7590b0b86c0b6d66c92709bd367603010b050b22
Adam Waterman <awaterman5895@gmail.com>
2019-11-01 15:46:13 +0000

    stripped back everything

--- venue/models.py
+++ venue/models.py
@@ -1,4 +1,3 @@
-from contact.models import Contact
 from django.db import models
 
 # Create your models here.
@@ -9,18 +8,8 @@ class BusinessType(models.Model):
     label = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
 
-
-class VenueStatus(models.Model):
-
-    label = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-
-
-class SocialMedia(models.Model):
-
-    website = models.CharField(max_length=250, null=True)
-    facebook = models.CharField(max_length=250, null=True)
-    twitter = models.CharField(max_length=250, null=True)
+    def __str__(self):
+        return self.label
 
 
 class Venue(models.Model):
@@ -36,12 +25,15 @@ class Venue(models.Model):
     latitude = models.DecimalField(max_digits=10, decimal_places=7)
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
 
-    contacts = models.ManyToManyField(Contact)
+    website = models.CharField(max_length=250, null=True)
+    facebook = models.CharField(max_length=250, null=True)
+    twitter = models.CharField(max_length=250, null=True)
+
+    phone = models.CharField(max_length=15)
+    email = models.CharField(max_length=250)
 
     product_location = models.CharField(max_length=500)
     business_type = models.ForeignKey(BusinessType, on_delete=models.PROTECT)
-    venue_status = models.ForeignKey(VenueStatus, on_delete=models.PROTECT)
-    social_media = models.ForeignKey(SocialMedia, on_delete=models.PROTECT)
 
     toilet = models.BooleanField(null=True)
     stock = models.BooleanField(null=True)
@@ -64,4 +56,4 @@ class Venue(models.Model):
     sunday_close = models.TimeField(null=True)
 
     def __str__(self):
-        return '{} {}'.format(self.id, self.name)
+        return self.name
