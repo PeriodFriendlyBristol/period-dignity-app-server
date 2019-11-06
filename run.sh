79b2e5cf70a548a70004f3874633ad247e91925b
Adam Waterman <awaterman5895@gmail.com>
2019-11-06 18:06:02 +0000

    fixed docker

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ sleep 3
 python3 manage.py migrate
 
 # TODO admin user must be made manually in production
-python3 manage.py shell < server/tools/create_admin.py
+python3 manage.py shell < tools/create_admin.py
 
 python3 manage.py runserver 0.0.0.0:8000
--- venue/migrations/0002_auto_20190930_1117.py
+++ venue/migrations/0002_auto_20190930_1117.py
@@ -69,7 +69,7 @@ def import_csv_data(apps, schema_editor):
         else:
             return social_media_lookup[social_media_hash]
 
-    with open('/code/server/venue/initial_venue_data.csv') as csv_file:
+    with open('/code/venue/initial_venue_data.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
 
         for row in csv_reader:
