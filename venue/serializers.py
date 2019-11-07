ceadb6659c1b25d9581bc3938f0d891013535427
Charlie <charlie.gillions@bristolisopen.com>
2019-11-07 21:56:24 +0000

    Added serialization library so that location is returned as latitude / longitude

--- venue/serializers.py
+++ venue/serializers.py
@@ -6,6 +6,7 @@ Docs https://www.django-rest-framework.org/api-guide/serializers/#modelserialize
 
 from rest_framework import serializers
 from .models import Venue, BusinessType
+from drf_extra_fields.geo_fields import PointField
 
 class BusinessTypeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,6 +16,7 @@ class BusinessTypeSerializer(serializers.ModelSerializer):
 
 class VenueSerializer(serializers.ModelSerializer):
     business_type = BusinessTypeSerializer(read_only=True)
+    location = PointField()
     class Meta:
         model = Venue
         fields = '__all__'
