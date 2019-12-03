42b28311d4fbf31fc73237a11c3f5722bfdaff64
Charlie <charlie.gillions@bristolisopen.com>
2019-12-03 13:48:31 +0000

    Ensured map on admin page (when creating new venues) centres over Bristol. Ensured non-required fields are not required on the admin page.

--- venue/admin.py
+++ venue/admin.py
@@ -1,5 +1,14 @@
-from django.contrib import admin
-from .models import Venue, BusinessType
+from venue.models import Venue, BusinessType
+from django.contrib.gis import admin
 
-admin.site.register(Venue)
+
+# Create a custom OpenStreetMaps admin page whose
+# map centers over Bristol when adding a location.
+class CustomOSMAdmin(admin.OSMGeoAdmin):
+    default_lat = 6702086.73
+    default_lon = -288083.71
+    default_zoom = 14
+
+
+admin.site.register(Venue, CustomOSMAdmin)
 admin.site.register(BusinessType)
